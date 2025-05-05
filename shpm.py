779cdb2b7bd0abd1d9eb856f5995f4520432901c
lachlanjs <43869608+lachlanjs@users.noreply.github.com>
2025-05-05 04:22:09 +0000

    Added the grassmann pair mean, crossover, and started assembly

--- shpm.py
+++ shpm.py
@@ -7,6 +7,22 @@ import pymanopt
 from pymanopt.manifolds import Product, Stiefel, Grassmann, Euclidean
 from pymanopt.manifolds.manifold import Manifold
 
+def normalise(x: np.array):
+
+    return x / np.linalg.norm(x)
+
+def grassmann_average(x_1: np.array, x_2: np.array):
+
+    a = 0.5 * (x_1 + x_2)
+
+    a_norm = np.linalg.norm(a)
+
+    if a_norm < 1e-6:
+        # avoid numerical instability from the upcoming division
+        return np.array([x_1[1], -x_1[0]])
+
+    return a / a_norm
+
 class SHPM(Manifold):
 
     """ Schur-Hurwitz Product Manifold
@@ -25,18 +41,16 @@ class SHPM(Manifold):
         
         if n % 2 != 0:
             raise ValueError("need n to be divisible by 2")
+        
+        self.n = n
 
         name = f"Hurwitz-Schur Product Manifold of dimension {n}"
         dimension = n**2 + 1
 
         self.stief_m = Stiefel(n, n)
-        self.T_D_gra_ad_pm = Grassmann(2, 1, n//2)
-        self.T_D_euc_ad_pm = Product([
-            Euclidean((1,)) for i in range(n//2)
-        ])
-        self.T_D_euc_bc_pm = Product([
-            Euclidean((2,)) for i in range(n//2)
-        ])
+        self.T_D_gra_ad_pm = Grassmann(2, 1, n//2)        
+        self.T_D_euc_ad_pm = Euclidean((n//2,))
+        self.T_D_euc_bc_pm = Euclidean((n//2, 2))
         self.T_U_euc_pm = Product([
             Euclidean((n-2-i,))
             for i in range(n-2)
@@ -85,13 +99,16 @@ class SHPM(Manifold):
         We just break all the rules here, because we're rebels
         - Q's are matrix multiplied
         - Grasmmanians are averaged on the unit sphere
-            - Just take average and normalise
+            - Just take average and normalise with the vector representations that are closer together
         - Euclidean points are averaged
         """
         
         return (
             P_1[0] @ P_2[0],
-
+            [normalise(0.5 * (g1 + g2) if np.dot(g1, g2) > 0 else 0.5 * (g1 - g2)) for g1, g2 in zip(P_1[1], P_2[1])],
+            self.T_D_euc_ad_pm.pair_mean(P_1[2], P_2[2]),
+            self.T_D_euc_bc_pm.pair_mean(P_1[3], P_2[3]),
+            self.T_U_euc_pm.pair_mean(P_1[4], P_2[4])
         )
     
     def assemble(self, P):
@@ -101,6 +118,27 @@ class SHPM(Manifold):
             p (_type_): point on the HPM manifold
         """
 
-        return
+        n = self.n
+        T = np.zeros((n, n))
+
+        # convert the Grassmann points
+        ## ensure that they are facing in the correct half of the plane (a < -d)
+        for idx, p_gra in enumerate(P[1]):
+            if p_gra[0] >= -p_gra[1]:
+                P[1][idx] = -1.0 * np.array(p_gra[0], p_gra[1])
+        
+        # place (a, d) and (b, c) on diagonal:
+        for d_idx in range(0, n, 2):
+            # (a, d)
+            T[d_idx, d_idx] = P[1][d_idx//2][0]         * P[2][d_idx//2]
+            T[d_idx + 1, d_idx + 1] = P[1][d_idx//2][1] * P[2][d_idx//2]
+            # (b, c)
+            T[d_idx + 1, d_idx] = P[2][d_idx//2][0] * P[2][d_idx//2]
+            T[d_idx, d_idx + 1] = P[2][d_idx//2][0] * P[2][d_idx//2]
+
+        # place the upper diagonal  
+        
+        
+        return P[0] @ T @ P[0].T
     
     
